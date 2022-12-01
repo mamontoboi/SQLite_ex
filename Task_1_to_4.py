bb2b07238beca72ad9540bc57bc8e5c66f1852ed
Mamontoboi <mamontoboi@gmail.com>
2022-12-02 00:04:34 +0100

    Updates to funcs were made.

--- Task_1_to_4.py
+++ Task_1_to_4.py
@@ -17,6 +17,82 @@ from deductions import deductions
 from total_deduct_income_calcs import summary
 
 
+def found_all(num):
+    try:
+        sqlite_connection = sqlite3.connect('monefy.db')
+        cursor = sqlite_connection.cursor()
+
+        sql_query = """SELECT * FROM profits_and_deductions WHERE amount >= :num;"""
+        data = {'num': num}
+        cursor.execute(sql_query, data)
+        print(cursor.fetchall())
+
+    except sqlite3.Error as e:
+        print(f"The following exception has occurred: {e}")
+
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+
+
+def total_exps(month):
+    try:
+        sqlite_connection = sqlite3.connect('monefy.db')
+        cursor = sqlite_connection.cursor()
+
+        sql_query = """SELECT SUM(amount) FROM profits_and_deductions 
+                        WHERE amount < 0 
+                        AND strftime('%m', time)=:month;"""
+        data = {'month': month}
+        cursor.execute(sql_query, data)
+        x = cursor.fetchone()
+        print(f"The total amount of monthly expenses is {abs(x[0])} dineros.")
+
+    except sqlite3.Error as e:
+        print(f"The following exception has occurred: {e}")
+
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+
+
+def delete_entry(id):
+    try:
+        sqlite_connection = sqlite3.connect('monefy.db')
+        cursor = sqlite_connection.cursor()
+
+        sql_query = """DELETE FROM profits_and_deductions WHERE id=:id;"""
+        data = {'id': id}
+        cursor.execute(sql_query, data)
+        sqlite_connection.commit()
+        print(f"The entry No. {id} was successfully deleted.")
+
+    except sqlite3.Error as e:
+        print(f"The following exception has occurred: {e}")
+
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+
+
+def delete_table():
+    try:
+        sqlite_connection = sqlite3.connect('monefy.db')
+        cursor = sqlite_connection.cursor()
+
+        sql_query = """DROP TABLE profits_and_deductions;"""
+        cursor.execute(sql_query)
+        sqlite_connection.commit()
+        print(f"The table was successfully deleted.")
+
+    except sqlite3.Error as e:
+        print(f"The following exception has occurred: {e}")
+
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+
+
 def create_new_db():
     try:
         sqlite_connection = sqlite3.connect('monefy.db')
@@ -51,7 +127,10 @@ if __name__ == '__main__':
         3 - Make a deduction
         4 - Total income per month
         5 - Total deductions per month
-        6 - Exit the programme\n
+        6 - Delete entry
+        7 - Found entries more than value
+        8 - Delete table
+        9 - Exit the programme\n
         """)
         match quest:
             case '1':
@@ -67,8 +146,14 @@ if __name__ == '__main__':
             case '4':
                 summary('+')
             case '5':
-                summary('-')
+                total_exps(input('Write the number of the month:\n'))
             case '6':
+                delete_entry(input('Write an id number of the entry you want to delete:\n'))
+            case '7':
+                found_all(input('Write the value all entries above what you want to see:\n'))
+            case '8':
+                delete_table()
+            case '9':
                 print("Thank you for choosing our software!")
                 break
             case _:
--- deductions.py
+++ deductions.py
@@ -12,11 +12,11 @@ def deductions(purpose, amount):
 
         sqlite_query = """INSERT INTO profits_and_deductions
         (purpose, amount, time)
-        VALUES (?, ?, ?);"""
+        VALUES (:purpose, :amount, :time);"""
 
         int_sum = amount * -1
         time_now = datetime.datetime.now()
-        data = (purpose, int_sum, time_now.strftime('%Y-%m-%d %H:%M'))
+        data = {'purpose': purpose, 'amount': int_sum, 'time': time_now.strftime('%Y-%m-%d %H:%M')}
         cursor.execute(sqlite_query, data)
         print(f"Entry was successfully added. The deduction of {amount} dineros for the {purpose} was recorded.\n")
 
